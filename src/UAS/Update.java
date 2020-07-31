8ddfcab5d6442f139fe6caac5dc972bd998a57ce
utrodus <come.programmer@gmail.com>
2020-07-31 14:44:50 +0700

    Insert Data & Read with Jframe Table

---
+++ src/UAS/Update.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan13;
+
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author utrodus
+ */
+public class Update {
+
+    Koneksi konek = new Koneksi();
+
+    public void update(int nim, String nama, String alamat, String jk) {
+
+        try {
+
+            konek.koneksi();
+            Statement statement = konek.con.createStatement();
+
+            
+            String sql_nama = "update identitas set nama='" + nama + "'where nim='" + nim + "'";
+            String sql_alamat = "update identitas set alamat='" + alamat + "'where nim='" + nim + "'";
+            String sql_jenis = "update identitas set jeniskelamin='" + jk + "'where nim='" + nim + "'";
+
+            
+            statement.executeUpdate(sql_nama);
+            statement.executeUpdate(sql_alamat);
+            statement.executeUpdate(sql_jenis);
+            statement.close();
+
+            JOptionPane.showMessageDialog(null, "Berhasil Diubah");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }
+
+}
